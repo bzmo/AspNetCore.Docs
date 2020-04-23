65e8bdcba2481cc2585d1c661fd3dc7a837e6e96
bzmo <bzmo@github.com>
2020-04-23 13:51:44 -0700

    Update and rename genClients.sh to genClients.ps1

--- aspnetcore/tutorials/web-api-help-pages-using-swagger/genClients.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker pull swaggerapi/swagger-codegen-cli
-
-SWAGGER_FILE"./swagger.json"
-CLIENTS_DIR"./clients"
-
-docker run --rm -v  generate -i $SWAGGER_FILE -l typescript-angular -o "${CLIENTS_DIR}/client-ts"
-swagger-codegen generate -i $SWAGGER_FILE -l csharp -o "${CLIENTS_DIR}/client-csharp"
