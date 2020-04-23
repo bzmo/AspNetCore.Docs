934605ae5a617d9e9e3b1acadc4af2a69a073418
bzmo <bzmo@github.com>
2020-04-23 13:49:57 -0700

    Delete genClients.ps1

--- genClients.ps1
+++
@@ -1,8 +0,0 @@
-﻿
-$LANGS = @("csharp","typescript-angular")
-$SWAGGER_FILE = "https://bzmo.github.io/AspNetCore.Docs/swagger.json"
-
-foreach ($lang in $LANGS) {
-    $cmd = "docker run -v ${PWD}:/local openapitools/openapi-generator-cli generate -i ${SWAGGER_FILE} -g ${lang} -o /local/clients/${lang}-client"
-    iex $cmd
-}
